4a94b745373bdd6a70a3590fcb9f6d4f5b18569f
thidiyas <thidiyas3d@gmail.com>
2020-04-04 13:57:10 +0530

    geolocation error handling completed
    error handling implemented, geolocation enableHighAccuracy added

--- weatherApp.js
+++ weatherApp.js
@@ -1,11 +1,10 @@
-// code is in-active now!
-
+//Author :Thidiyas Limbu
 
 
 //check if geolocation is supported
 if (navigator.geolocation) { 
-	// Get the user's current position
-  navigator.geolocation.getCurrentPosition(success, error);
+	// Get the user's current position, on success calls the success() method
+  navigator.geolocation.getCurrentPosition(success, error,{enableHighAccuracy:"true"});
   console.log(" geo location is supported");
   } else {
 	alert('Geolocation is not supported in your browser');
@@ -16,13 +15,36 @@ if (navigator.geolocation) {
 function success(position){
   var lat = position.coords.latitude;
   //console.log(lat);
-  console.log("cool")
+  console.log("cool");
   apiRequest( position.coords.latitude, position.coords.longitude); 
 }
 
-function error(){} //error handling code pending
 
+//error handling code 
+function error(e){
+  var x = document.getElementById("temp");
+  x.style.fontSize="20px";
+  x.style.color = "red";
+    switch(e.code){
+    case e.PERMISSION_DENIED:
+      //DOM UPDATE
+      x.innerHTML = "!PERMISSION DENIED";
+      break;
+    case e.POSITION_UNAVAILABLE:
+      
+      x.innerHTML = "LOCATION UNAVAILABLE";
+      break;
+    case e.TIMEOUT:
+      
+      x.innerHTML = "REQUEST TIMEOUT";
+      break;
+    case e.UNKNOWN_ERROR:
+      x.innerHTML = "ERROR OCCURED";
+      break;
+  }
+} 
 
+//XMLHttp request to fetch weather data
 function apiRequest(lat, lon){
   var Http = new XMLHttpRequest();
   var url='https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lon;
@@ -34,9 +56,14 @@ function apiRequest(lat, lon){
        console.log(Http.responseText);
        var data = JSON.parse(Http.responseText);
        document.getElementById("city-country").innerHTML = data.name+ ", " +data.sys.country ;
-       document.getElementById("temp").innerHTML = data.main.temp +" &deg C";
-       document.getElementById("main").innerHTML = data.weather[0].main;
-       document.getElementById("icon").src = data.weather[0].icon;
+       document.getElementById("desc").innerHTML = data.weather[0].description;
+       document.getElementById("temp").innerHTML = data.main.temp +" &deg";
+       document.getElementById("inspiration").innerHTML = "inspired by freecodecamp.org";
+       //document.getElementById("icon").src = data.weather[0].icon;
+       
+      //document.getElementById("icon").innerHTML = "<img src='"+img_url+"'>";
+       
+       
         }
    
       }
